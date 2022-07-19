431c90ee5962ac9ee104879a58696733fbd51ce5
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 20:43:45 -0500

    TESTINGCODE

---
+++ testing code/testing code/testing_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_codeApp.swift
+//  testing code
+//
+//  Created by MAC on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct testing_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
